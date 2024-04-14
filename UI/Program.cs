f35333880a85e7382385b3299e5ba44a11824eaa
karyle anne <karyle anne@LAPTOP-GSQAHJBR>
2024-04-13 21:58:24 -0900

    Add project files.

---
+++ UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using UserAccountBusiness;
+
+namespace UserDataUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter UserName: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter UserID: ");
+            string userID = Console.ReadLine();
+
+
+            AccountService userData = new AccountService();
+
+            
+            if (true)
+            {
+
+                Console.WriteLine("Welcome User!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
